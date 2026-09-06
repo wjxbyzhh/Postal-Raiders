973bb3e14f09800b7602993256dfa6209fde1458
wjxbyzhh <wjxbyzhh@outlook.com>
2026-09-06 19:20:58 +0800

    添加 Architectury 多加载器模板

---
+++ common/src/main/java/com/github/wjxbyzhh/postal-raiders/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.github.wjxbyzhh.postal-raiders;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "postal-raiders";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/github/wjxbyzhh/postal-raiders/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.github.wjxbyzhh.postal-raiders.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.github.wjxbyzhh.postal-raiders.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
